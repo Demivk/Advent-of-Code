7d9ecd63df654ca76de13a6717e351c6ca091075
Demi <demi.van.kesteren@gmail.com>
2022-12-02 20:18:03 +0100

    Add day 2 part 1 and 2

---
+++ src/day2/day2.clj
@@ -0,0 +1,48 @@
+(ns day2.day2
+  (:require
+    [clojure.string :as string]))
+
+(defn read-input [] (flatten (map #(string/split-lines %) (string/split (slurp "src/day2/input.edn") #"\n\n"))))
+
+(def win-inputs #{"A Y" "B Z" "C X"})
+(def draw-inputs #{"A X" "B Y" "C Z"})
+(def lose-inputs #{"A Z" "B X" "C Y"})
+
+(defn shape->score [shape] (case shape "X" 1 "Y" 2 "Z" 3))
+
+(defn input->part-1-score [input]
+  (let [[_ shape] (string/split input #" ")
+        shape-score (shape->score shape)
+        outcome-score (cond
+                        (contains? lose-inputs input) 0
+                        (contains? draw-inputs input) 3
+                        (contains? win-inputs input) 6)]
+    (+ shape-score outcome-score)))
+
+(defn part-1 []
+  (->>
+    (read-input)
+    (mapv #(input->part-1-score %))
+    (apply +)))
+
+(defn find-shape [opponent outcome-inputs]
+  (some
+    #(let [splitted (string/split % #" ")]
+       (when (= (first splitted) opponent) (second splitted)))
+    outcome-inputs))
+
+(defn input->part-2-score [input]
+  (let [[opponent shape] (string/split input #" ")
+        shape-score (shape->score
+                      (case shape
+                        "X" (find-shape opponent lose-inputs)
+                        "Y" (find-shape opponent draw-inputs)
+                        "Z" (find-shape opponent win-inputs)))
+        outcome-score (case shape "X" 0 "Y" 3 "Z" 6)]
+    (+ shape-score outcome-score)))
+
+(defn part-2 []
+  (->>
+    (read-input)
+    (mapv #(input->part-2-score %))
+    (apply +)))
--- src/user.clj
+++ src/user.clj
@@ -1,11 +1,14 @@
 (ns user
   (:require
     [clojure.tools.namespace.repl :refer [refresh]]
-    [day1.day1 :as day-1]))
+    [day1.day1 :as day-1]
+    [day2.day2 :as day-2]))
 
 (def go refresh)
 
 (comment
   go
   (day-1/part-1)
-  (day-1/part-2))
+  (day-1/part-2)
+  (day-2/part-1)
+  (day-2/part-2))

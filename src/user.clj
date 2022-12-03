71bd0ea794cb4d8374a30b7d7830ba141a45ca6d
Demi <demi.van.kesteren@gmail.com>
2022-12-03 09:33:34 +0100

    Add day 3 part 1 and 2

--- src/user.clj
+++ src/user.clj
@@ -2,7 +2,8 @@
   (:require
     [clojure.tools.namespace.repl :refer [refresh]]
     [day1.day1 :as day-1]
-    [day2.day2 :as day-2]))
+    [day2.day2 :as day-2]
+    [day3.day3 :as day-3]))
 
 (def go refresh)
 
@@ -11,4 +12,6 @@
   (day-1/part-1)
   (day-1/part-2)
   (day-2/part-1)
-  (day-2/part-2))
+  (day-2/part-2)
+  (day-3/part-1)
+  (day-3/part-2))

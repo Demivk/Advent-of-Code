c16a0b477425c9226a2193912b0d4a73139fa1df
Demi <demi.van.kesteren@gmail.com>
2022-12-09 16:44:19 +0100

    Add day 8 part 1 and 2

--- src/user.clj
+++ src/user.clj
@@ -7,7 +7,8 @@
     [day4.day4 :as day-4]
     [day5.day5 :as day-5]
     [day6.day6 :as day-6]
-    [day7.day7 :as day-7]))
+    [day7.day7 :as day-7]
+    [day8.day8 :as day-8]))
 
 (def go refresh)
 
@@ -26,4 +27,6 @@
   (day-6/part-1)
   (day-6/part-2)
   (day-7/part-1)                                            ; No idea how I managed to do day 7
-  (day-7/part-2))
+  (day-7/part-2)
+  (day-8/part-1)                                            ; Needed help with this one (in clojure), thanks Reddit
+  (day-8/part-2))

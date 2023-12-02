02d2262e409a86d69d074aa0e75dfc172ffdccd1
Demi van Kesteren <demi.van.kesteren@gmail.com>
2023-12-02 09:52:06 +0100

    2023 - Day 2 part 1 and 2

--- src/utils.clj
+++ src/utils.clj
@@ -4,6 +4,8 @@
 
 (defn input-file-path [year day] (str "src/year" year "/day" day "/input.edn"))
 
-(defn read-rows [input] (flatten (map #(string/split-lines %) (string/split input #"\n\n"))))
+(defn read-rows [input] (flatten (map #(string/split-lines %) (string/split input #"\n"))))
 
 (defn parse-int [s] (Integer/parseInt s))
+
+(defn split-whitespace [s] (string/split s #" "))
--- src/year2023/calendar.clj
+++ src/year2023/calendar.clj
@@ -2,7 +2,8 @@
   (:require
     [clojure.test :refer [testing is]]
     [clojure.tools.namespace.repl :refer [refresh]]
-    [year2023.day1.puzzles :as day-1]))
+    [year2023.day1.puzzles :as day-1]
+    [year2023.day2.puzzles :as day-2]))
 
 (def go refresh)
 
@@ -10,3 +11,6 @@
 
 (testing "Day 1 - part 1" (is (= (day-1/part-1) 55029)))
 (testing "Day 1 - part 2" (is (= (day-1/part-2) 55686)))
+
+(testing "Day 2 - part 1" (is (= (day-2/part-1) 2331)))
+(testing "Day 2 - part 2" (is (= (day-2/part-2) 71585)))
